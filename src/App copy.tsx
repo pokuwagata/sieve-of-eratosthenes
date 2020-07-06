db6b7fd5bb592eb207e5016f7a061759f81bf82d
pokuwagata <pokuwagata@gmail.com>
2020-07-07 07:22:10 +0900

    処理を整理

--- src/App copy.tsx
+++ src/App copy.tsx
@@ -1,40 +1,47 @@
 import React, { useEffect } from "react";
 
-const eratosthenes = (max: number): number[] => {
-  const isPrime = [];
-  for (let i = 0; i <= max; i++) {
-    isPrime[i] = true;
-  }
-  const primes: number[] = [];
-  for (let i = 2; i * i <= max; i++) {
-    if (isPrime[i]) {
-      for (let j = 2; i * j <= max; j++) {
-        isPrime[i * j] = false;
-      }
-    }
-  }
-  isPrime.forEach((isP, i) => {
-    if (isP && i >= 2) {
-      primes.push(i);
-    }
-  });
-
-  return primes;
-};
-
 const App = () => {
   const [nums, setNums] = React.useState<number[]>([1]);
-  const [maxNum, setMaxNum] = React.useState<number>(10);
+  const [maxNum, setMaxNum] = React.useState<number|string>(1);
   const [pnums, setpnums] = React.useState<number[]>([]);
-  const [input, setInput] = React.useState<string>("10");
   const [numToColor, setnumToColor] = React.useState<string[]>([]);
 
-  useEffect(() => {
-    const ns = Array.from(Array(maxNum + 1).keys());
+  useEffect(()=>{
+    const init = 15
+    setMaxNum(init)
+    const n = Number(init)
+    const ns = Array.from(Array(n + 1).keys());
     ns.shift();
     setNums(ns);
-    setpnums(eratosthenes(maxNum));
-  }, [maxNum]);
+    setpnums(eratosthenes(n)); 
+  }, [])
+
+  const eratosthenes = (max: number): number[] => {
+    const isPrime = [];
+    for (let i = 0; i <= max; i++) {
+      isPrime[i] = true;
+    }
+    const primes: number[] = [];
+    for (let i = 2; i * i <= max; i++) {
+      if (isPrime[i]) {
+        const colors = ["#F1948A", "#7FB3D5", "#F7DC6F", "#B2BABB"];
+        numToColor[i] = colors[i % 4];
+        for (let j = 2; i * j <= max; j++) {
+          isPrime[i * j] = false;
+          if (numToColor[i * j]) continue;
+          numToColor[i * j] = colors[i % 4];
+        }
+        setnumToColor([...numToColor]);
+      }
+    }
+    isPrime.forEach((isP, i) => {
+      if (isP && i >= 2) {
+        primes.push(i);
+      }
+    });
+
+    return primes;
+  };
 
   const num = (props: { num: number }) => {
     const { num } = props;
@@ -58,23 +65,24 @@ const App = () => {
     <div className="App">
       <input
         type="number"
-        value={input}
+        value={maxNum}
         onChange={(e) => {
-          if (e.target.value === "") {
-            setInput("");
+          if(e.target.value === '') {
+            setMaxNum('')
             return;
           }
           let n = Number(e.target.value);
           if (n <= -1 || n >= 1e4) n = 0;
           setMaxNum(n);
-          setInput(e.target.value);
+          const ns = Array.from(Array(n + 1).keys());
+          ns.shift();
+          setNums(ns);
+          setpnums(eratosthenes(n));
         }}
       ></input>
       <div>
         <p>prime numbers</p>
-        <div
-          style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
-        >
+        <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
           {pnums.map((num) => (
             <div key={num} style={{ marginRight: "1em" }}>
               {num}
